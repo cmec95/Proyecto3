7a377bc6630ff0c19d6b5bbf81d81209eb8b58f3
cmec95 <140910148+cmec95@users.noreply.github.com>
2023-11-09 17:54:41 -0600

    graficaCovid

---
+++ Ejercicio 4/assets/js/index.js
@@ -0,0 +1,30 @@
+console.log("Entro a index.js")
+
+//EDNPOINT https://cat-fact.herokuapp.com
+fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=all")
+.then(response => response.json())
+.then(datosApi => {
+  console.log(datosApi);
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['cases',],
+      datasets: [{
+        label: 'Casos COVID-19',
+        data: datosApi.cases,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  
+});
+
